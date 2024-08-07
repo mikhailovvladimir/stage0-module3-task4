94978a19e59d18d05003ce6e3cb0453ad8a30381
CtaRik <1602211@mail.ru>
2024-08-08 00:09:57 +0600

    tasks has done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,16 +2,14 @@ package school.mjc.stage0.module3.task4;
 import java.util.logging.Logger;
 
 public class IntRepresentation {
-    Logger logger = Logger.getLogger(getClass().getName());
-
     public static void main(String[] args) {
         char c = 'c';
         char h = 'h';
         char a = 'a';
         char r = 'r';
-        System.out.println((int)c);
-        System.out.println((int)h);
-        System.out.println((int)a);
-        System.out.println((int)r);
+        System.out.println((short)c);
+        System.out.println((short)h);
+        System.out.println((short)a);
+        System.out.println((short)r);
     }
 }
