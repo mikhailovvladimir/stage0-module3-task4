ad7ebd745d71e2ecd4267ba33bcdee0ddd3ab333
CtaRik <1602211@mail.ru>
2024-08-07 22:25:26 +0600

    tasks has done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 1.0;
+        double b = 2.0;
+
+        double firstPart = 9 * Math.pow(a, 2) - 5 * b + 2 + a - 7;
+
+        double secondPart = (a + b - 4 * a * b) / 2;
+
+        double result = firstPart * secondPart;
+
+        System.out.println("Результат формулы: " + result);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = (int) 'c';
+        int h = (int) 'h';
+        int a = (int) 'a';
+        int r = (int) 'r';
+
+        System.out.println(c); // выводит 99
+        System.out.println(h); // выводит 104
+        System.out.println(a); // выводит 97
+        System.out.println(r); // выводит 114
     }
 }
