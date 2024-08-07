c443c6d4ae9fd0346397d18e8998d8bc7870cce3
CtaRik <1602211@mail.ru>
2024-08-08 00:35:42 +0600

    tasks has done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,13 +2,15 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char c = 'c';
-        char h = 'h';
-        char a = 'a';
-        char r = 'r';
-        System.out.println((short)c);
-        System.out.println((short)h);
-        System.out.println((short)a);
-        System.out.println((short)r);
+        char one = 99;
+        char two = 104;
+        char three = 97;
+        char four = 114;
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
     }
+
 }
